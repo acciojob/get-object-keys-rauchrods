6c580b65cdf1986bef5536f1c620057d49d3e764
Rauch Rodrigues <94990103+rauchrods@users.noreply.github.com>
2023-04-14 10:29:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+class Student{
+    constructor(name, age, grade){
+        this.name = name;
+        this.age = age;
+        this.grade = grade;
+    }
+}
+Student.prototype.getKeys = function(){
+    return Object.keys(this);
+}
+
+let student = new Student("john",24,7.4);
+console.log(student);
+console.log(student.getKeys());
